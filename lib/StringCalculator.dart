cdff2d69bac1a90b4999dd5a045702dac595b9b5
Ridham <ridham2244@gmail.com>
2025-02-08 14:00:26 +0530

    improve add method for sum

--- lib/StringCalculator.dart
+++ lib/StringCalculator.dart
@@ -3,12 +3,7 @@ class StringCalculator {
     if (numbers.isEmpty) return 0;
     String delimiter = ",";
     List<String> parts = numbers.replaceAll("\n", delimiter).split(delimiter);
-    int sum = 0;
-
-    for (String num in parts) {
-      sum += int.parse(num);
-    }
-
+    int sum = parts.where((num) => num.isNotEmpty).map(int.parse).reduce((a, b) => a + b);
     return sum;
   }
 }
