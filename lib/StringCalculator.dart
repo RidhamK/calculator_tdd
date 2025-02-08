fc2e63164bd5a47a65b4c2eb0f54a5031014fad6
Ridham <ridham2244@gmail.com>
2025-02-08 14:04:13 +0530

    improve add method for new test case

--- lib/StringCalculator.dart
+++ lib/StringCalculator.dart
@@ -2,6 +2,14 @@ class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
     String delimiter = ",";
+    if (numbers.startsWith("//")) {
+      RegExp delimiterRegex = RegExp(r"//(.+)\n");
+      Match? match = delimiterRegex.firstMatch(numbers);
+      if (match != null) {
+        delimiter = match.group(1)!;
+        numbers = numbers.substring(match.end);
+      }
+    }
     List<String> parts = numbers.replaceAll("\n", delimiter).split(delimiter);
     int sum = parts.where((num) => num.isNotEmpty).map(int.parse).reduce((a, b) => a + b);
     return sum;
