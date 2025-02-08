a735937da1ce65622943b34ce243e040d30af7e0
Ridham <ridham2244@gmail.com>
2025-02-08 13:48:28 +0530

    allow new line in with RegExp

--- lib/StringCalculator.dart
+++ lib/StringCalculator.dart
@@ -2,7 +2,7 @@ class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
 
-    List<String> parts = numbers.split(',');
+    List<String> parts = numbers.split(RegExp(r'[,\\n]+'));
     int sum = 0;
 
     for (String num in parts) {
